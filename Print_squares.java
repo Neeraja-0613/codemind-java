abd14d119c72afa8fdff773917c3053a96f278df
Neeraja-0613 <23P31A4430@acet.ac.in>
2024-08-14 10:43:39 +0530

    class Codechef

---
+++ Print_squares.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+        System.out.printf("1 - %d",1*1);
+        System.out.printf("2 - %d",2*2);
+        System.out.printf("3 - %d",3*3);
+        System.out.printf("4 - %d",4*4);
+        System.out.printf("5 - %d",5*5);
+	}
+}
