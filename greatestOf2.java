3b7cadf46ac3262a69d134db95413b40e0f97fcc
Neeraja-0613 <23P31A4430@acet.ac.in>
2024-08-19 13:45:21 +0530

    import java.util.Scanner

---
+++ greatestOf2.java
@@ -0,0 +1,16 @@
+//greatest of 2 numbers:
+import java.util.Scanner;
+public class number
+{
+    public static void main(String[] args)
+    {
+        int n,x;
+        Scanner read= new Scanner(System.in);
+        n=read.nextInt();
+        x=read.nextInt();
+        if(n>x)
+        System.out.println(n);
+        else
+        System.out.println(x);
+    }
+}
