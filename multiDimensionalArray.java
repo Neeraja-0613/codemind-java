88b2902be4ecf3efc8d67d44e0f9115c05a74bba
Neeraja Palla <23P31A4430@acet.ac.in>
2024-09-23 09:59:49 +0530

    import java.util.Scanner

---
+++ multiDimensionalArray.java
@@ -0,0 +1,19 @@
+public class multiDimensionalArrays {
+    public static void main(String[] args)
+    {
+        int[][] arr={{10,20}, {30,40,50,60}, {70,80,90}, {10,20,30,40,50,60}};
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+        System.out.println(arr[1].length);
+        System.out.println(arr[2].length);
+        System.out.println(arr[3].length);
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                System.out.println(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
